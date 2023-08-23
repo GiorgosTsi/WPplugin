7d81ae974e99c36ee35de31da1be9c1449111942
Giorghs <giorgostsikri@hotmail.com>
2023-08-23 15:31:37 +0300

    Added the UI for the settings page

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -8,6 +8,7 @@
 
 namespace Inc\Pages;
 
+use Inc\API\callbacks\ManagerCallbacks;
 use Inc\API\callbacks\AdminCallbacks;
 use Inc\Base\BaseController;
 use Inc\API\SettingApi;
@@ -26,10 +27,14 @@ class Admin extends BaseController
 
 	public $callbacks;
 
+	public $callbacks_mngr;
+
 	//add action for the admin page
 	public function register(){
 
 		$this->callbacks =  new AdminCallbacks();
+
+		$this->callbacks_mngr = new ManagerCallbacks();
 		
 		$this->pageBuilder = new SettingApi();
 
@@ -110,18 +115,59 @@ class Admin extends BaseController
 
 
 	public function setSettings(){
+		/*
+		Setting format:
+			arg1) string, the option group 
+			arg2) the option name.
+			arg3) callback function which is gonna be called when the get_option(option_name) method is called.
+		 */
 
 		$settings = array(
-
-						array(
-							"option_group" => 'giorghs_option_group',
-							"option_name"  => 'text1',
-							"callback"     => array($this->callbacks ,'settingCallback')
-						),
-						array(
-							"option_group" => 'giorghs_option_group',
-							"option_name"  => 'text2'
-						)
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'cpt_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'taxonomy_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'media_widget',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'gallery_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'testimonial_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'templates_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'login_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'membership_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			),
+			array(
+				'option_group' => 'giorghs_option_group',
+				'option_name' => 'chat_manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+			)
 		);
 
 		//set the settings of the SettingApi class
@@ -131,16 +177,23 @@ class Admin extends BaseController
 
 
 	public function setSections(){
+		/*
+		Section format:
+			arg1) id, the id of the section 
+			arg2) Title printed for the section
+			arg3) Callback function when section is printed
+			arg4) main page slug
+		 */
 
 		$sections = array(
-						array(
-							"id"       => 'set1_id',                                  //The id of the section in the section page
-							"title"    => 'Settings',								  //Title printed for the section
-							"callback" => array($this->callbacks ,'sectionCallback'), //Callback function when section completed
-							"page"     => 'giorghs_plugin'                            //main page slug
-
-						)
+			array(
+				'id' => 'giorghs_admin_index',
+				'title' => 'Settings Manager',
+				'callback' => array( $this->callbacks_mngr, 'adminSectionManager' ),
+				'page' => 'giorghs_plugin'
+			)
 		);
+
 
 		//set the sections of the SettingApi class
 		$this->pageBuilder->setSections($sections);
@@ -150,30 +203,117 @@ class Admin extends BaseController
 
 	public function setFields(){
 
-		$fields = array(
-						array(
-						'id' 	   => 'text1',                                   //id of the field
-						'title'    => 'Text Example',							 //title printed of this field
-						'callback' => array( $this->callbacks, 'fieldCallback' ),//callback func
-						'page'     => 'giorghs_plugin',							 //main page slug
-						'section'  => 'set1_id',                                 //section's id
-						'args'     => array(                                     //args of the field
-									'label_for' => 'text1',
-									'class' => 'example-class'
-								)
-					    ),
+		/*
+		Fields format:
+			arg1) id, the id of the field
+			arg2) title printed of this field
+			arg3) callback func
+			arg4) main page slug
+			arg5) to id tou section sto opoio anhkei
+			arg6) args of callback function.We need 'label for' to be identical with the option_name.
+				  so inside the checkboxField function to know if the checkbox is checked or no.
+		 */
 
-					    array(
-						'id' 	   => 'text2',                                    //id of the field
-						'title'    => 'Fname',							          //title printed of this field
-						'callback' => array( $this->callbacks, 'fieldCallback2' ),//callback func
-						'page'     => 'giorghs_plugin',							  //main page slug
-						'section'  => 'set1_id',                                  //section's id
-						'args'     => array(                                      //args of the field
-									'label_for' => 'text2',
-									'class' => 'example-class'
-								)
-					    )
+		$fields = array(
+			array(
+				'id' => 'cpt_manager',
+				'title' => 'Activate CPT Manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'cpt_manager',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'taxonomy_manager',
+				'title' => 'Activate Taxonomy Manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'taxonomy_manager',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'media_widget',
+				'title' => 'Activate Media Widget',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'media_widget',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'gallery_manager',
+				'title' => 'Activate Gallery Manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'gallery_manager',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'testimonial_manager',
+				'title' => 'Activate Testimonial Manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'testimonial_manager',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'templates_manager',
+				'title' => 'Activate Templates Manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'templates_manager',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'login_manager',
+				'title' => 'Activate Ajax Login/Signup',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'login_manager',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'membership_manager',
+				'title' => 'Activate Membership Manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'membership_manager',
+					'class' => 'ui-toggle'
+				)
+			),
+			array(
+				'id' => 'chat_manager',
+				'title' => 'Activate Chat Manager',
+				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+				'page' => 'giorghs_plugin',
+				'section' => 'giorghs_admin_index',
+				'args' => array(
+					'label_for' => 'chat_manager',
+					'class' => 'ui-toggle'
+				)
+			)
 		);
 
 		//set the fields of the SettingApi class

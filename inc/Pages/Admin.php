2dfdd537cbdacb7c922d486f3f2dbaace3afd12c
Giorghs <giorgostsikri@hotmail.com>
2023-08-24 10:14:07 +0300

    Code cleanup to avoid repetition while adding settings

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -121,54 +121,64 @@ class Admin extends BaseController
 			arg2) the option name.
 			arg3) callback function which is gonna be called when the get_option(option_name) method is called.
 		 */
+		$settings = array();
 
-		$settings = array(
-			array(
+		foreach ( $this->managers as $key => $value ) {
+			$settings[] = array(
 				'option_group' => 'giorghs_option_group',
-				'option_name' => 'cpt_manager',
+				'option_name' => $key,
 				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'taxonomy_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'media_widget',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'gallery_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'testimonial_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'templates_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'login_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'membership_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			),
-			array(
-				'option_group' => 'giorghs_option_group',
-				'option_name' => 'chat_manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
-			)
-		);
+			);
+		}
+
+
+		// $settings = array(
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'cpt_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'taxonomy_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'media_widget',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'gallery_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'testimonial_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'templates_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'login_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'membership_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	),
+		// 	array(
+		// 		'option_group' => 'giorghs_option_group',
+		// 		'option_name' => 'chat_manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxSanitize' )
+		// 	)
+		// );
 
 		//set the settings of the SettingApi class
 		$this->pageBuilder->setSettings($settings);
@@ -214,107 +224,123 @@ class Admin extends BaseController
 				  so inside the checkboxField function to know if the checkbox is checked or no.
 		 */
 
-		$fields = array(
-			array(
-				'id' => 'cpt_manager',
-				'title' => 'Activate CPT Manager',
+		$fields = array();
+
+		foreach ( $this->managers as $key => $value ) {
+			$fields[] = array(
+				'id' => $key,
+				'title' => $value,
 				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
 				'page' => 'giorghs_plugin',
 				'section' => 'giorghs_admin_index',
 				'args' => array(
-					'label_for' => 'cpt_manager',
+					'label_for' => $key,
 					'class' => 'ui-toggle'
 				)
-			),
-			array(
-				'id' => 'taxonomy_manager',
-				'title' => 'Activate Taxonomy Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'taxonomy_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'media_widget',
-				'title' => 'Activate Media Widget',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'media_widget',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'gallery_manager',
-				'title' => 'Activate Gallery Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'gallery_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'testimonial_manager',
-				'title' => 'Activate Testimonial Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'testimonial_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'templates_manager',
-				'title' => 'Activate Templates Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'templates_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'login_manager',
-				'title' => 'Activate Ajax Login/Signup',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'login_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'membership_manager',
-				'title' => 'Activate Membership Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'membership_manager',
-					'class' => 'ui-toggle'
-				)
-			),
-			array(
-				'id' => 'chat_manager',
-				'title' => 'Activate Chat Manager',
-				'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
-				'page' => 'giorghs_plugin',
-				'section' => 'giorghs_admin_index',
-				'args' => array(
-					'label_for' => 'chat_manager',
-					'class' => 'ui-toggle'
-				)
-			)
-		);
+			);
+		}
+
+		// $fields = array(
+		// 	array(
+		// 		'id' => 'cpt_manager',
+		// 		'title' => 'Activate CPT Manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'cpt_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'taxonomy_manager',
+		// 		'title' => 'Activate Taxonomy Manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'taxonomy_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'media_widget',
+		// 		'title' => 'Activate Media Widget',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'media_widget',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'gallery_manager',
+		// 		'title' => 'Activate Gallery Manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'gallery_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'testimonial_manager',
+		// 		'title' => 'Activate Testimonial Manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'testimonial_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'templates_manager',
+		// 		'title' => 'Activate Templates Manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'templates_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'login_manager',
+		// 		'title' => 'Activate Ajax Login/Signup',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'login_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'membership_manager',
+		// 		'title' => 'Activate Membership Manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'membership_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	),
+		// 	array(
+		// 		'id' => 'chat_manager',
+		// 		'title' => 'Activate Chat Manager',
+		// 		'callback' => array( $this->callbacks_mngr, 'checkboxField' ),
+		// 		'page' => 'giorghs_plugin',
+		// 		'section' => 'giorghs_admin_index',
+		// 		'args' => array(
+		// 			'label_for' => 'chat_manager',
+		// 			'class' => 'ui-toggle'
+		// 		)
+		// 	)
+		//);
 
 		//set the fields of the SettingApi class
 		$this->pageBuilder->setFields($fields);
